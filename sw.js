974b210e4d2a6b8e5f711d27f72e8d6ceb42bf23
Shubham <shubham.tagline@gmail.com>
2021-07-14 15:53:53 +0530

    Updates

--- sw.js
+++ sw.js
@@ -4,6 +4,9 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
       cache.addAll([
+        "/",
+        "/favicon.ico",
+        "manifest.json",
         "/static/js/bundle.js",
         "/static/js/main.chunk.js",
         "/static/js/1.chunk.js",
@@ -13,19 +16,23 @@ self.addEventListener("install", (event) => {
         "/favicon.ico",
         "/static/media/tenor.706124a2.gif",
         "/static/js/vendors~main.chunk.js",
-        "index.html",
-        "/",
+        "/index.html",
         "/signIn",
-        "/Examination-Demo/static/js/3.9bfb649b.chunk.js",
-        "/Examination-Demo/static/js/0.ddf4a8f4.chunk.js",
-        "/Examination-Demo/static/js/8.1ed39376.chunk.js",
-        "/Examination-Demo/static/js/main.331a3e04.chunk.js",
+        "/static/js/3.9bfb649b.chunk.js",
+        "/static/js/0.ddf4a8f4.chunk.js",
+        "/static/js/8.1ed39376.chunk.js",
+        "/static/js/main.331a3e04.chunk.js",
       ]);
     })
-  );
-});
+    );
+  });
+  // "/Examination-Demo/static/js/3.9bfb649b.chunk.js",
+  // "/Examination-Demo/static/js/0.ddf4a8f4.chunk.js",
+  // "/Examination-Demo/static/js/8.1ed39376.chunk.js",
+  // "/Examination-Demo/static/js/main.331a3e04.chunk.js",
 
 self.addEventListener("fetch", (event) => {
+  console.log(`fetch`)
   if(!navigator.onLine){
     event.respondWith(
       caches.match(event.request).then((result) => {
@@ -34,5 +41,4 @@ self.addEventListener("fetch", (event) => {
     );
   }
 });
-
  
