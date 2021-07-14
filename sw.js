b8e37bebe740da93e97fe510611f0edeaf40aff2
Shubham <shubham.tagline@gmail.com>
2021-07-14 17:16:27 +0530

    Updates

--- sw.js
+++ sw.js
@@ -3,25 +3,30 @@ let cacheData = "appV1";
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
-      cache.addAll([
-        "/",
-        "/favicon.ico",
-        "/manifest.json",
-        "/static/js/bundle.js",
-        "/static/js/main.chunk.js",
-        "/static/js/1.chunk.js",
-        "/static/js/0.chunk.js",
-        "/static/js/9.chunk.js",
-        "/static/js/8.chunk.js",
-        "/static/media/tenor.706124a2.gif",
-        "/static/js/vendors~main.chunk.js",
-        "/index.html",
-        "/signIn",
+      "/",
         "/static/js/3.9bfb649b.chunk.js",
         "/static/js/0.ddf4a8f4.chunk.js",
         "/static/js/8.1ed39376.chunk.js",
         "/static/js/main.331a3e04.chunk.js",
-      ]);
+        cache.addAll([
+          "/",
+          "/favicon.ico",
+          "/manifest.json",
+          "/static/js/bundle.js",
+          "/static/js/main.chunk.js",
+          "/static/js/1.chunk.js",
+          "/static/js/0.chunk.js",
+          "/static/js/9.chunk.js",
+          "/static/js/8.chunk.js",
+          "/static/media/tenor.706124a2.gif",
+          "/static/js/vendors~main.chunk.js",
+          "/index.html",
+          "/signIn",
+          "/static/js/3.9bfb649b.chunk.js",
+          "/static/js/0.ddf4a8f4.chunk.js",
+          "/static/js/8.1ed39376.chunk.js",
+          "/static/js/main.331a3e04.chunk.js",
+        ]);
     })
     );
   });
