aa77fd548fc1b7906a5fe1642bf83d16a78682c1
Shubham <shubham.tagline@gmail.com>
2021-07-14 10:13:18 +0530

    Updates

---
+++ static/js/21.408d24ca.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpexam=this.webpackJsonpexam||[]).push([[21],{98:function(e,t,c){"use strict";c.r(t);var a=c(0),n=c(2),s=c(4);t.default=function(){var e=Object(n.f)();return Object(a.useEffect)((function(){localStorage.clear(),e.push("/")}),[e]),Object(s.jsx)(s.Fragment,{})}}}]);
+//# sourceMappingURL=21.408d24ca.chunk.js.map
