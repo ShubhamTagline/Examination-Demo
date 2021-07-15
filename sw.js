32a2b38f9c807d1f7ed14cf9d163dcbaa84f0510
Shubham <shubham.tagline@gmail.com>
2021-07-15 09:55:51 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 let cacheData = "appV1";
 
-this.addEventListener("install", (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
        cache.addAll([
         "/",
-        "/favicon.ico",
         "/index.html",
-        "/signIn",
+        "/favicon.ico",
+        "/static/media/tenor.706124a2.gif",
         "/Examination-Demo/static/js/3.9bfb649b.chunk.js",
         "/Examination-Demo/static/js/0.ddf4a8f4.chunk.js",
         "/Examination-Demo/static/js/8.1ed39376.chunk.js",
@@ -23,15 +23,15 @@ this.addEventListener("install", (event) => {
   // "/static/js/0.chunk.js",
   // "/static/js/9.chunk.js",
   // "/static/js/8.chunk.js",
-  // "/static/media/tenor.706124a2.gif",
   // "/static/js/vendors~main.chunk.js",
+  // "/signIn",
   // "/static/js/3.9bfb649b.chunk.js",
   // "/static/js/0.ddf4a8f4.chunk.js",
   // "/static/js/8.1ed39376.chunk.js",
   // "/static/js/main.331a3e04.chunk.js",
 
-
-this.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", (event) => {
+  console.log(`fetch`)
   if(!navigator.onLine){
     event.respondWith(
       caches.match(event.request).then((result) => {
