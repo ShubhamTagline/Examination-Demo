3f19a19e3632c9e9686b561af1e177a7c9eebfa0
Shubham <shubham.tagline@gmail.com>
2021-07-15 10:08:28 +0530

    change sw

--- public/sw.js
+++ public/sw.js
@@ -4,17 +4,16 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
        cache.addAll([
-         "/",
-         "/Examination-Demo/index.html",
+         "/Examination-Demo/public/index.html",
          "/Examination-Demo/favicon.ico",
-         "/Examination-Demo/App.js",
-         "/Examination-Demo/index.js",
+         "/Examination-Demo/src/App.js",
+         "/Examination-Demo/src/index.js",
          "/Examination-Demo/static/media/tenor.706124a2.gif",
          "/Examination-Demo/static/js/3.9bfb649b.chunk.js",
          "/Examination-Demo/static/js/0.ddf4a8f4.chunk.js",
          "/Examination-Demo/static/js/8.1ed39376.chunk.js",
          "/Examination-Demo/static/js/main.331a3e04.chunk.js",
-         "/manifest.json",
+         "/Examination-Demo/public/manifest.json",
        ]);
     })
     );
